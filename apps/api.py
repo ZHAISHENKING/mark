9fded1c132fa8191aac4a4464ae26413fed35610
ZHAISHENKING <18700790825@163.com>
2019-01-18 23:57:33 +0800

    str to html

--- apps/api.py
+++ apps/api.py
@@ -12,6 +12,7 @@ from flask_restful import Resource
 from util.common import trueReturn, falseReturn, ms, catch_exception
 from .models import Mark, UrlConfig
 from mongoengine.queryset.visitor import Q
+import html
 
 
 class CreateMarkAPI(Resource):
@@ -39,19 +40,26 @@ class CreateMarkAPI(Resource):
         # )
         # mark.save()
         result = """
-            # {}
-            ----
-            `{}` **{}**
-            输入
-            ```json
-            {}
-            ```
-            输出
-            ```json
-            {}
-            ```
-            
-        """
+        <pre>
+# {}
+----
+
+`{}` **{}**
+
+输入
+
+```json
+{}
+```
+
+输出
+
+```json
+{}
+</pre>
+```"""
+
         result = result.format(data["title"], data["method"], return_url+data["path"], data["input"], data["output"])
-        return result
+        print(result)
+        return make_response(result)
 
